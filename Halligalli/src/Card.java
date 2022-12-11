c738a5f1316968da3a921d3b6b086aca31394c72
cholee445 <cholee227@naver.com>
2022-12-11 19:14:40 +0900

    Card class and CardDeck class

--- Halligalli/src/Card.java
+++ Halligalli/src/Card.java
@@ -1,5 +1,26 @@
 
 public class Card {
 	
+	private String fruit;
+	private int face;
+	
+	public static final int SIZE_OF_FIVE_FRUIT = 1;
+	public static final int SIZE_OF_FOUR_FRUIT = 2;
+	public static final int SIZE_OF_THREE_FRUIT = 3;
+	public static final int SIZE_OF_TWO_FRUIT = 3;
+	public static final int SIZE_OF_ONE_FRUIT = 5;
+	
+	public static final String BANANA = "banana";
+	public static final String STRAWBERRY = "strawberry";
+	public static final String LIME = "lime";
+	public static final String PLUM = "plum";
+	
+	public Card(String s, int r) {
+		fruit = s; face = r;
+	}
+	
+	public int face() { return face; } // 자신의 과일 개수 알려줌
+	
+	public String fruit() { return fruit; } // 자신의 과일 종류 알려줌
 	
 }
