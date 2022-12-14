83b4abd2be451f16a028351ded3c2331b1634f59
cholee445 <cholee227@naver.com>
2022-12-14 17:52:02 +0900

    add Starter class

--- Halligalli/src/Starter.java
+++ Halligalli/src/Starter.java
@@ -1,4 +1,17 @@
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Starter {
+public class Starter extends JButton implements ActionListener{
 	
+	private Frame frame;
+	
+	public Starter(Frame f) {
+		super("Start");
+		frame = f;
+		addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		frame.clickedStart();
+	}
 }
